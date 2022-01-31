f63e3a96e2122729d177b734a3f322023c4a581f
Bird <39280243+jlambert360@users.noreply.github.com>
2022-01-31 11:49:55 -0500

    Update build-mac.sh

--- Resources/build-mac.sh
+++ Resources/build-mac.sh
@@ -7,12 +7,13 @@ DATA_SYS_PATH="./Data/Sys/"
 BINARY_PATH="./build/Binaries/Dolphin.app/Contents/Resources/"
 
 # Move into the build directory, run CMake, and compile the project
+mv -f -v /home/runner/work/birdbuilddolphin/birdbuilddolphin/Ishiiruka/* /home/runner/work/birdbuilddolphin/birdbuilddolphin/
 mkdir -p build
 pushd build
 cmake ${CMAKE_FLAGS} ..
 #Copy wx folder to Source/Core/
-cp -R /Users/runner/work/birdbuilddolphin/birdbuilddolphin/Ishiiruka/Externals/wxWidgets3/include/wx /Users/runner/work/birdbuilddolphin/birdbuilddolphin/Ishiiruka/build/Source/Core/
-cp /Users/runner/work/birdbuilddolphin/birdbuilddolphin/Ishiiruka/Externals/wxWidgets3/wx/* /Users/runner/work/birdbuilddolphin/birdbuilddolphin/Ishiiruka/build/Source/Core/wx/
+cp -R /Users/runner/work/birdbuilddolphin/birdbuilddolphin/Externals/wxWidgets3/include/wx /Users/runner/work/birdbuilddolphin/birdbuilddolphin/build/Source/Core/
+cp -R /Users/runner/work/birdbuilddolphin/birdbuilddolphin/Externals/wxWidgets3/wx/* /Users/runner/work/birdbuilddolphin/birdbuilddolphin/build/Source/Core/wx/
 make -j7
 popd
 
